ba60967ee88040344e5d0905c52223f4ef1b87f6
Santhosh T R <santhoshtr0912@gmail.com>
2025-08-19 22:26:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,10 @@ const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/evoting';
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['https://digicastt-votingg.vercel.app'], // 
+  credentials: true, // optional if you use cookies
+}));
 app.use(express.json());
 
 // MongoDB connection
